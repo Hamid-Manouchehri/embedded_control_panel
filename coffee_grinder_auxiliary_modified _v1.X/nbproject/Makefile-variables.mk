4ac41c13cdb4d049d68cf75545a23821c14dd3fb
hamid manouchehri <hamid.manouchehri.njf@gmail.com>
2022-10-02 13:04:01 +0330

    Add files via upload

---
+++ coffee_grinder_auxiliary_modified _v1.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=coffee_grinder_auxiliary_modified__v1.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/coffee_grinder_auxiliary_modified__v1.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=coffeegrinderauxiliarymodifiedv1.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/coffeegrinderauxiliarymodifiedv1.x.tar
